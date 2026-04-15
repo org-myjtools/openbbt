94648cfd8249a60693122bf1aed7012937488484
luis <luiinge@gmail.com>
2026-04-15 18:53:25 +0200

    chore: remove TUI module

--- openbbt-tui/src/main/java/module-info.java
+++
@@ -1,5 +0,0 @@
-module org.myjtools.openbbt.tui {
-    requires com.googlecode.lanterna;
-    requires org.myjtools.openbbt.core;
-    exports org.myjtools.openbbt.tui;
-}
--- openbbt-tui/src/main/java/org/myjtools/openbbt/tui/Main.java
+++
@@ -1,8 +0,0 @@
-package org.myjtools.openbbt.tui;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        TuiApp.launch();
-    }
-}
